446331a3801f9c278fe4e9325144bafd51f6ee4c
AmrAbouElkair <amrmahmoud20022002@gmail.com>
2023-09-20 19:36:32 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Category from "./components/Category/Category";
+import Footer from "./components/Footer/Footer";
+import Hero from "./components/Hero/Hero";
+import Menu from "./components/Menu/Menu";
+import NavBar from "./components/NavBar/NavBar";
+
+const App = () => {
+  return (
+    <main className="relative dark:bg-black duration-300">
+      <NavBar />
+      <section className="xl:padding-l xl:padding-r padding-y max-xl:px-2 max-sm:dark:pt-16">
+        <Hero />
+      </section>
+      <section className="xl:padding-l xl:padding-r padding-y !pt-0 max-xl:px-4">
+        <Menu />
+      </section>
+      <section className="xl:padding-l xl:padding-r padding-y max-xl:px-4 !pt-0">
+        <Category />
+      </section>
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
